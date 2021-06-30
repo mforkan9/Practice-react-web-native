32ded51b6d1e1f83a79c541fa9017c06f30885f2
mforkan9 <mforka9@gmail.com>
2021-07-01 00:06:19 +0600

    first commit

---
+++ src/components/Header/Header2.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header2 = () => {
+    return (
+        <div>
+            <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">Navbar</span>
+  </div>
+</nav>
+        </div>
+    );
+};
+
+export default Header2;
